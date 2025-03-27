1975150bacc3f28a1add5a8f4d069026490c843f
ArthurO2002 <arthurvoskanyan02@gmail.com>
2025-03-27 13:07:42 +0400

    FIX: Add form validation for task edit

--- resources/js/components/Tasks.vue
+++ resources/js/components/Tasks.vue
@@ -4,6 +4,9 @@ import { Pencil, Trash2 } from "lucide-vue-next";
 import {useTaskStore} from "../store/taskStore.js";
 import {useToast} from "vue-toastification";
 import {ref} from "vue";
+import * as yup from 'yup';
+import {useForm} from "vee-validate";
+
 
 const taskStore = useTaskStore();
 const toast = useToast();
@@ -15,9 +18,24 @@ const props = defineProps({
     }
 })
 
+const editSchema = yup.object({
+    title: yup.string().required('Task title is required'),
+    description: yup.string()
+        .max(500, 'Description cannot exceed 500 characters')
+        .nullable()
+});
+
+const { handleSubmit, errors: formErrors, defineInputBinds } = useForm({
+    validationSchema: editSchema,
+    initialValues: {
+        title: props.task.title,
+        description: props.task.description
+    }
+});
+
 const isEditing = ref(false);
-const editedTitle = ref(props.task.title);
-const editedDescription = ref(props.task.description);
+const editedTitle = defineInputBinds("title");
+const editedDescription = defineInputBinds("description");
 
 async function changeTaskStatus(event){
     try {
@@ -37,12 +55,12 @@ async function deleteTask() {
     }
 }
 
-async function saveEditedTask() {
+const onSave = handleSubmit(async (values) => {
     try {
         const updatedTask = {
             ...props.task,
-            title: editedTitle.value,
-            description: editedDescription.value,
+            title: values.title,
+            description: values.description
         };
         await taskStore.updateTask(updatedTask);
         isEditing.value = false;
@@ -50,7 +68,7 @@ async function saveEditedTask() {
     } catch (error) {
         toast.error("Something went wrong while updating the task.");
     }
-}
+});
 
 </script>
 
@@ -65,15 +83,19 @@ async function saveEditedTask() {
                     class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 />
             </div>
-            <div class="w-full" :class="{ 'line-through text-gray-400 dark:text-gray-200': props.task.status }">
-                <template v-if="!isEditing">
+            <div class="w-full">
+                <div v-if="!isEditing" :class="{ 'line-through text-gray-400 dark:text-gray-200': props.task.status }">
                     <h2 class="text-xl font-bold">{{ props.task.title }}</h2>
                     <p>{{ props.task.description }}</p>
-                </template>
+                </div>
                 <template v-else>
-                    <input v-model="editedTitle" class="bg-gray-50 border mb-3 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                    <textarea v-model="editedDescription" class="block p-2.5 w-full mb-3 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                    <button @click="saveEditedTask" class="text-white bg-yellow-400 hover:bg-yellow-800 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 focus:outline-none dark:focus:ring-yellow-800 disabled:opacity-50 disabled:cursor-not-allowed">Save</button>
+                    <input v-bind="editedTitle" class="bg-gray-50 border mb-3 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                    <span v-if="formErrors.title" class="text-red-500 text-sm">{{ formErrors.title }}</span>
+
+                    <textarea v-bind="editedDescription" class="block p-2.5 w-full mb-3 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                    <span v-if="formErrors.description" class="text-red-500 text-sm">{{ formErrors.description }}</span>
+
+                    <button @click="onSave" class="text-white bg-yellow-400 hover:bg-yellow-800 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 focus:outline-none dark:focus:ring-yellow-800 disabled:opacity-50 disabled:cursor-not-allowed">Save</button>
                     <button @click="isEditing = false" class="text-white bg-red-400 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800 disabled:opacity-50 disabled:cursor-not-allowed">Cancel</button>
                 </template>
             </div>
